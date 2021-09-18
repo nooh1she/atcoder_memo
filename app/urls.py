53b69955ecbb522c446417ebf48668371480c052
noda <kawazakanahuna@gmail.com>
2021-09-18 23:57:33 +0900

    views and urls modified.

---
+++ app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from app import views
+
+app_name = 'app'
+urlpatterns = [
+
+    #トップページ
+    path('', views.top, name = 'top'),
+
+    #問題の登録
+    path('submit', views.submit, name = 'submib'),
+
+    #問題の閲覧・検索
+    path('items', views.items, name = 'items')
+
+
+]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import get_list_or_404, render, redirect
-from django.views.generic.list
+from django.views.generic.list import ListView
+
+from app.models import Problem
 
 # Create your views here.
+def top(request):
+    return render(request, 'app/top.html')
+
+def submit(request):
+    return render(request, 'app/submit.html')
