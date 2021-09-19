2b5009781f4b9ddd014877ad2d67d622eeaf5d55
noda <kawazakanahuna@gmail.com>
2021-09-19 19:21:15 +0900

    m

--- atcoder_memo/settings.py
+++ atcoder_memo/settings.py
@@ -63,10 +63,19 @@ WSGI_APPLICATION = 'atcoder_memo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'private_diary',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
