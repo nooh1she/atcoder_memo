099dc0c3accf12dd3e28d0fdbd47f6b4a9311883
noda <kawazakanahuna@gmail.com>
2021-09-21 04:46:05 +0900

    final commit

--- atcoder_memo/settings.py
+++ atcoder_memo/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["codeTag.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
